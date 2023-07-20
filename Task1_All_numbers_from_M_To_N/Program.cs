75a67f5a16769a1e63aa0cdb7b23986927393f16
zyich <zuilinho@yandex.ru>
2023-07-20 14:40:42 +0200

    task 1 all numbers from N to 1

---
+++ Task1_All_numbers_from_M_To_N/Program.cs
@@ -0,0 +1,16 @@
+﻿/* 
+Задача 64: Задайте значение N. 
+Напишите программу, которая 
+выведет все натуральные числа 
+в промежутке от N до 1. 
+Выполнить с помощью рекурсии.
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1" 
+*/ 
+string ReturnRealDigits(int startNum)
+{ 
+    
+    if(startNum == 1) return startNum.ToString(); 
+    return startNum + " " + ReturnRealDigits( startNum-1); 
+} 
+Console.WriteLine(ReturnRealDigits(5));
