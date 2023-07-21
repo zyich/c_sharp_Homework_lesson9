c7908892d221200b8b766e27c9d0c6ebd988c283
zyich <zuilinho@yandex.ru>
2023-07-21 09:19:32 +0200

    task 3 ackermann recurse

---
+++ Task3_Ackermann_function/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+Задача 68: Напишите программу вычисления 
+функции Аккермана с помощью рекурсии. 
+Даны два неотрицательных числа m и n.
+m = 2, n = 3 -> A(m,n) = 9
+m = 3, n = 2 -> A(m,n) = 29
+*/
+
+int ack(int m,int n)
+{
+int ans;
+ if  (m == 0 ) ans = n+1;
+ else if (n == 0) ans = ack(m-1,1);
+ else ans = ack(m -1, ack(m,n-1));
+ return (ans);
+
+} 
+Console.WriteLine(ack(4,1));
